53d9f89a4a12ad9677703c4262e194cc896276ae
gegabo <gega.diez@gmail.com>
2024-11-05 17:04:50 +0100

    loss funcitno

--- network_gp.py
+++ network_gp.py
@@ -8,6 +8,17 @@ def _num_stabilize_diag(mat, stabilizer=1e-8):
     diag = torch.eye(mat.size(-1), device=mat.device).expand(mat.shape)
     return mat + stabilizer * diag
 
+
+def _cholesky_log_determinant(mat):
+    cholesky_decomposition = torch.linalg.cholesky(
+        _num_stabilize_diag(mat),
+    )
+    return 2 * torch.sum(torch.log(torch.diagonal(cholesky_decomposition)))
+
+
+
+
+
 class FeedForwardNN(nn.Module):
     """Simple linear NN with ReLU activation."""
     def __init__(self, layer_sizes):
@@ -176,6 +187,32 @@ class TEMPEST(nn.Module):
         self.kernel_nm = self.kernel.kernel_mat(t, self.inducing_points)
         self.kernel_mn = torch.transpose(self.kernel_nm, 0, 1)
 
+    def _compute_diagonal_kernel(self, precision):
+        """Compute the diagonal elements of the kernel matrix."""
+        return precision * (
+            self.kernel_nn - torch.diagonal(
+                torch.matmul(
+                    self.kernel_nm,
+                    torch.matmul(
+                        self.kernel_mm_inv,
+                        self.kernel_mn,
+                    ),
+                ),
+            ),
+        )
+
+    def _compute_Lambda(self):
+        """Compute the Lambda matrix."""
+        return torch.matmul(
+            self.kernel_mm_inv,
+            torch.matmul(
+                torch.matmul(
+                    self.kernel_nm.unsqueeze(2),
+                    torch.transpose(self.kernel_nm.unsqueeze(2), 1, 2),
+                ),
+                self.kernel_mm_inv,
+            ),
+        )
 
     def approximate_posterior(self, t, qzx_mu, qzx_var):
         constant = self.N_data / t.shape[0]
@@ -229,12 +266,56 @@ class TEMPEST(nn.Module):
         # GP_mean_vector and GP_mean_sigma is updated posterior, while mu_l and A_l acts more like a prior (compare eq 9 in Tian24)
 
 
-    def variational_loss(self, t, x, beta):
+    def variational_loss(self, qzx_mu, qzx_var):
         """
         Compute the Hensman loss term for the current batch.
-        Compare eg. Eq.(7) and (10) in Jazbec21
+        Compare eg. Eq.(7) and (10) in Jazbec21.
+        More details in Jazbec21 SI B, proposition B.1
         """
+        m = self.inducing_points.shape[0]
+        log_det_kmm = _cholesky_log_determinant(self.kernel_mm)
+        log_det_A = _cholesky_log_determinant
+        KL_div = 0.5 * (-m + torch.trace(torch.matmul(
+            self.kernel_mm_inv,
+            self.A_l,
+        )) + torch.sum(self.mu_l * torch.matmul(
+            self.kernel_mm_inv,
+            self.mu_l,
+        ) + log_det_kmm - log_det_A))
+        # compute L3 sum term
+        mean_vec = torch.matmul(
+            self.kernel_mn,
+            torch.matmul(
+                self.kernel_mm_inv,
+                self.mu_l,
+            )
+        )  # first term in Jazbec21 SI, B.1 first eq.
+        precision = 1 / qzx_var
+        k_iitilde = self._compute_diagonal_kernel(precision)
+        Lambda = self._compute_Lambda()
+        tr_ALambda = precision * torch.einsum(
+            'bii->b',
+            torch.matmul(
+                self.A_l,
+                Lambda,
+            ),
+        )
+        loss_term_L3 = -0.5 * torch.sum(
+            torch.sum(k_iitilde) + torch.sum(tr_ALambda) +
+            torch.sum(torch.log(qzx_var)) + m *
+            torch.log(2 * 3.1415927410125732) +
+            torch.sum(precision * (qzx_mu - mean_vec)**2)
+        )
+        return loss_term_L3, KL_div
 
+    def gauss_cross_entropy(mu_l, GP_mean_sigma, qzx_mu, qzx_var):
+        """Proof see SI Tian24, Proposition 4 on p.83"""
+        log2pi = 1.8378770664093453
+        log_qzx_var = torch.log(qzx_var)
+        scaled_square_diff = (
+            GP_mean_sigma + mu_l**2 - 2 * mu_l * qzx_mu + qzx_mu**2
+        ) / qzx_var
+        return - 0.5 * (log2pi + log_qzx_var + scaled_square_diff)
 
     def gp_step(self, x, t):
         qzx = self.encoder(x)
@@ -248,7 +329,10 @@ class TEMPEST(nn.Module):
                 qzx_mu[latent_dim],  # check dim of qzx_mu
                 qzx_var[latent_dim],
             )
-            self.variational_loss
+            self.variational_loss(
+                qzx_mu[latent_dim],  # check dim of qzx_mu
+                qzx_var[latent_dim],
+            )
 
 
 
