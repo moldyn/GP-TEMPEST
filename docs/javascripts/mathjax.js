fa82bcc5a771eb5220958b50a87cd52d570bee9a
gegabo <gega.diez@gmail.com>
2026-03-13 23:48:12 +0100

    boldsymbol support

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -3,7 +3,11 @@ window.MathJax = {
     inlineMath: [["$", "$"], ["\\(", "\\)"]],
     displayMath: [["$$", "$$"], ["\\[", "\\]"]],
     processEscapes: true,
-    processEnvironments: true
+    processEnvironments: true,
+    packages: {"[+]": ["boldsymbol"]}
+  },
+  loader: {
+    load: ["[tex]/boldsymbol"]
   }
 };
 
